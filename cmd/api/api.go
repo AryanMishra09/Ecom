62ba7b9fdd96d07316d88df2261ba5afdbd77979
Aryan Mishra <mishraaryan9204@gmail.com>
2025-09-01 11:09:46 +0530

    cart and order apis + readme

--- cmd/api/api.go
+++ cmd/api/api.go
@@ -5,6 +5,7 @@ import (
 	"log"
 	"net/http"
 
+	"github.com/AryanMishra09/Ecom/service/product"
 	"github.com/AryanMishra09/Ecom/service/user"
 	"github.com/gorilla/mux"
 )
@@ -28,6 +29,18 @@ func (s *APIServer) Run() error {
 	userStore := user.NewStore(s.db)
 	userHandler := user.NewHandler(userStore)
 	userHandler.RegisterRoutes(subRouter)
+
+	productStore := product.NewStore(s.db)
+	productHandler := product.NewHandler(productStore, userStore)
+	productHandler.RegisterRoutes(subRouter)
+
+	orderStore := order.NewStore(s.db)
+
+	cartHandler := cart.NewHandler(productStore, orderStore, userStore)
+	cartHandler.RegisterRoutes(subrouter)
+
+	// Serve static files
+	router.PathPrefix("/").Handler(http.FileServer(http.Dir("static")))
 
 	log.Printf("Listening on: %v", s.addr)
 
